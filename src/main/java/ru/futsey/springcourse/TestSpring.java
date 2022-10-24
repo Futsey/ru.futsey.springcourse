ff96f51872fcc8a65a1a2cfeddada9479e0acb54
Fut <shmopls@gmail.com>
2022-10-24 16:06:40 +0500

    Dependency injection (XML configeration)

--- src/main/java/ru/futsey/springcourse/TestSpring.java
+++ src/main/java/ru/futsey/springcourse/TestSpring.java
@@ -8,13 +8,12 @@ public class TestSpring {
      * Конфигурация SpringApp посредством XML файла конфигурации
      * @param args
      * @author ANDREW PETRUSHIN
-     * @version 1.0
+     * @version 1.1
      */
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
 
         context.close();
